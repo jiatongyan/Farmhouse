b8aa13b2ee5fe1ba4ea67f73f5c27f8ccd1616a5
dylan <2857015121@qq.com>
2026-08-13 16:53:39 +0800

    b

---
+++ src/test/java/com/dylan/farmhouse/FarmhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.farmhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmhouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
